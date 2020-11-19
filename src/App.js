549fbb66610f96da67e935298a5a7eea05a0d7a1
Ryan Garza <ryangarza@live.com>
2020-11-18 20:18:25 -0600

    sort and search functions working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import './App.css';
 
+import UserRow from "./components/UserRow";
+
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [originalUsers, setOriginalUsers] = useState([]);
+
+  useEffect(() => {
+      axios.get("https://randomuser.me/api/?results=10&nat=us")
+      .then((res) => {
+        setOriginalUsers(res.data.results);  
+        setUsers(res.data.results);
+      });
+  },[]);
+
+  let sortByName = () => {
+    users.sort(function(a, b,){
+      return a.name.last.localeCompare(b.name.last);
+    });
+    setUsers([...users]); 
+  };
+
+  let handleInputChange = (e) => {
+    let searchTerm = e.target.value.toLowerCase();
+    let filteredUsers = originalUsers.filter(user => user.name.last.toLowerCase().includes(searchTerm));
+    setUsers([...filteredUsers]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <td>image</td>
+          <td>name<button onClick={sortByName}>Sort</button></td>
+          <td>phone</td>
+          <td>email</td>
+          <td>DOB</td>
+        </tr>
+        {users.map((user) => {
+          return (
+            <UserRow user={user} />
+          )
+        })}
+      </table>
+      <input
+          onChange={handleInputChange}
+          type="text"
+          className="form-control"
+          placeholder="Search users"
+          id="term"
+        />
     </div>
   );
 }
---
+++ src/components/UserRow.jsx
@@ -0,0 +1,21 @@
+const UserRow = (props) => {
+
+    const {user} = props;
+    const image = user.picture.large;
+    const name = user.name.first + " " + user.name.last;
+    const phone = user.phone;
+    const email = user.email;
+    const dob = user.dob.date;
+
+    return (
+        <tr>
+            <td><img src={image} alt=""/></td>
+            <td>{name}</td>
+            <td>{phone}</td>
+            <td>{email}</td>
+            <td>{dob}</td>
+        </tr>
+    )
+}
+
+export default UserRow;
